b318c9fe34e82d001133a685a5ad01f6a2e65059
Satoshi Nemoto <s23c1104tl@s.chibakoudai.jp>
2024-12-06 10:18:23 +0900

    add

---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import find_packages, setup
+
+package_name = 'mypkg'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='Satoshi Nemoto',
+    maintainer_email='miraiprj3104@icloud.com.jp',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'talker = mypkg.talker:main', #talker.pyのmain関数という意味
+            'listener = mypkg.listener:main',
+        ],
+    },
+)
