628cc015a5216f9a4368a6fe09683033432cc7af
Satoshi Nemoto <s23c1104tl@s.chibakoudai.jp>
2024-12-21 23:28:53 +0900

    add commnd

--- mypkg/talker.py
+++ mypkg/talker.py
@@ -1,24 +1,21 @@
 import datetime
 import rclpy
 from rclpy.node import Node
-from std_msgs.msg import Int32
+from std_msgs.msg import Int16
 
 rclpy.init()
 node = Node("talker")
-pub = node.create_publisher(Int32, "countup", 10)
+pub = node.create_publisher(Int16, "countup", 10)
 n = 0
 
 
 def cb():
     global n
-    msg = Int32()
+    msg = Int16()
     msg.data = n
     pub.publish(msg)
     dt_now = datetime.datetime.now()
-    time = dt_now.hour * 10000
-    time += dt_now.minute * 100
-    time += dt_now.second
-    n = time
+    n = dt_now.second
 
 
 def main():
--- test/test.bash
+++ test/test.bash
@@ -1,4 +1,4 @@
-#!/bin/bash -xv
+#!/bin/bash
 
 dir=~
 [ "$1" != "" ] && dir="$1"
@@ -7,7 +7,8 @@ cd $dir/ros2_ws
 colcon build
 source $dir/.bashrc
 timeout 5 ros2 launch mypkg talk_listen.launch.py > /tmp/mypkg.log
-today=$(date +%H%M%S)
+now=$(date +%S)
+now=$((now - 1))
 
 cat /tmp/mypkg.log |
-grep "${today}"
+grep "${now}"
