15934ded1b300320598338975039417b2596b141
Satoshi Nemoto <s23c1104tl@s.chibakoudai.jp>
2024-12-24 15:33:38 +0900

    test

--- test/test.bash
+++ test/test.bash
@@ -8,7 +8,7 @@ colcon build
 source $dir/.bashrc
 timeout 5 ros2 launch mypkg talk_listen.launch.py > /tmp/mypkg.log
 now=$(TZ='Asia/Tokyo' date +%H%M%S)
-now=$((now))
+now=$((now-2))
 
 cat /tmp/mypkg.log |
 grep "${now}"
