59396f0f6de28bcb338e4be3ed7c9aa6cd4fb14b
Satoshi Nemoto <s23c1104tl@s.chibakoudai.jp>
2024-12-26 20:00:50 +0900

    test

--- test/test.bash
+++ test/test.bash
@@ -8,7 +8,7 @@ dir=~
 cd $dir/ros2_ws
 colcon build
 source $dir/.bashrc
-timeout 5 ros2 launch mypkg talk_listen.launch.py > /tmp/mypkg.log
+timeout 10 ros2 launch mypkg talk_listen.launch.py > /tmp/mypkg.log
 timejp=$(TZ='Asia/Tokyo' date +%H%M%S)
 timejp=$((timejp-2))
 
